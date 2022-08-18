18ebb853d1e44c89dbb2a19546a1adf6339552df
AlexandrVladlenovich <Mystafin_Alexandr@mail.ru>
2022-08-18 16:13:24 +0500

    add Zadacha025_A^B

---
+++ Zadacha025_A^B/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите переменную A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите переменную B");
+int b = Convert.ToInt32(Console.ReadLine());
+int b2 = b;
+int a2 = a;
+while (b > 1)
+    {
+      b = b - 1;
+      a = a * a2;
+    }
+Console.WriteLine(+ a2 + " в степени " + b2 + " = " + a);
