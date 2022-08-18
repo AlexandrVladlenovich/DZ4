4ea9b112c41157b93dd03b121a48f1061d2e5ad3
AlexandrVladlenovich <Mystafin_Alexandr@mail.ru>
2022-08-18 20:15:31 +0500

    add Zadacha27_SumCifrChisla

---
+++ Zadacha27_SumCifrChisla/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число:");
+string number = Console.ReadLine();
+int sum = 0;
+foreach (var item in number)
+{
+    sum += Convert.ToInt32(item.ToString());
+}
+Console.WriteLine("Ответ:" +sum);
